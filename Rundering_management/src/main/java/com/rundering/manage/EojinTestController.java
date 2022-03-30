47b688ae9b592f98a2aec059ba130710bed8da04
EOJIN <rornwl4512@gmail.com>
2022-03-30 12:21:55 +0900

    본사 - 지점발주내역, 발주상세 jsp 분리 완료

--- Rundering_management/src/main/java/com/rundering/manage/EojinTestController.java
+++ Rundering_management/src/main/java/com/rundering/manage/EojinTestController.java
@@ -7,8 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EojinTestController {
 
 	@RequestMapping("/admin/branchOrder")
+	public String branchOrderList() {
+		return "admin/branch_equipment_order_list";
+	}
+	@RequestMapping("/admin/branchOrder/detail")
 	public String branchOrder() {
-		return "admin/branch_equipment_order";
+		return "admin/branch_equipment_order_detail";
 	}
 
 }
