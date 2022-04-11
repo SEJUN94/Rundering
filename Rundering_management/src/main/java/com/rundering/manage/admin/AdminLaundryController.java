0b9531a91883bd72917f4bff25ba453bfc645e69
EOJIN <rornwl4512@gmail.com>
2022-04-11 14:00:44 +0900

    본사 - AdminLaundryController수정

--- Rundering_management/src/main/java/com/rundering/manage/admin/AdminLaundryController.java
+++ Rundering_management/src/main/java/com/rundering/manage/admin/AdminLaundryController.java
@@ -7,26 +7,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class AdminLaundryController {
 	
-	@RequestMapping("/laundryorder/list")
+	@RequestMapping("/laundryorder/list.do")
 	public String laundryOrderList() {
 		return "admin/laundryorder/laundry_order_list";
 	}
-	@RequestMapping("/laundryorder/detail")
+	@RequestMapping("/laundryorder/detail.do")
 	public String laundryOrderDetail() { 
 		return "admin/laundryorder/laundry_order_detail";
 	}
 	
-	@RequestMapping("/laundryitems/list")
+	@RequestMapping("/laundryitems/list.do")
 	public String laundryItemsList() {
 		return "admin/laundryitems/laundryitems_list";
 	}
 	
-	@RequestMapping("/laundryitems/regist")
+	@RequestMapping("/laundryitems/regist.do")
 	public String laundryItemRegist() {
 		return "admin/laundryitems/laundryitems_regist";
 	}
 	
-	@RequestMapping("/laundryitems/modify")
+	@RequestMapping("/laundryitems/modify.do")
 	public String laundryItemModify() {
 		return "admin/laundryitems/laundryitems_modify";
 	}
