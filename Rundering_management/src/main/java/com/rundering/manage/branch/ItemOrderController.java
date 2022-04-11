83dfbea35e140310fef9335a88d9d82e16fbe9a6
PC-03 <PC-03@back.com>
2022-04-11 15:48:06 +0900

    item-_-

--- Rundering_management/src/main/java/com/rundering/manage/branch/ItemOrderController.java
+++ Rundering_management/src/main/java/com/rundering/manage/branch/ItemOrderController.java
@@ -10,26 +10,26 @@ public class ItemOrderController {
 	
 	@RequestMapping("/list")
 	private String list() {
-		String url= "/branch/iteamorder/iteamorder_list";
+		String url= "/branch/itemorder/itemorder_list";
 		return url;
 	}
 	
 	
 	@RequestMapping("/detail")
 	private String detail() {
-		String url= "/branch/iteamorder/iteamorder_detail";
+		String url= "/branch/itemorder/itemorder_detail";
 		return url;
 	}
 	
 	@RequestMapping("/regist")
 	private String regist() {
-		String url= "/branch/iteamorder/iteamorder_regist";
+		String url= "/branch/itemorder/itemorder_regist";
 		return url;
 	}
 	
 	@RequestMapping("detail/modify")
 	private String modify() {
-		String url = "/branch/iteamorder/modify";
+		String url = "/branch/itemorder/modify";
 		return url;
 	
 	}
