6edd81d88819fb2bd7f5e63a5515e38e3e7ba65c
PC-04 <PC-04@DESKTOP-B62PM0J>
2022-04-16 16:15:54 +0900

    orderGoods -> laundryArticles 수정

--- Rundering_management/src/main/java/com/rundering/dao/AttachDAOImpl.java
+++ Rundering_management/src/main/java/com/rundering/dao/AttachDAOImpl.java
@@ -1,5 +1,7 @@
 package com.rundering.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 
 import com.rundering.dto.AttachVO;
@@ -10,15 +12,21 @@ public class AttachDAOImpl implements AttachDAO{
 	public void setSession(SqlSession session) {
 		this.session = session;
 	}
-	
-	@Override
-	public void insertOrderGoodsAtach(AttachVO attach) throws Exception {
-		session.update("Attach-Mapper.insertOrderGoodsAtach",attach);
-	}
 	@Override
 	public int selectFileNo() throws Exception {
 		int seq= session.selectOne("Attach-Mapper.selectFileNo");
 		return seq;
+	}
+
+	@Override
+	public void insertOrderGoodsAtach(AttachVO attach) throws Exception {
+		session.update("Attach-Mapper.insertOrderGoodsAttach",attach);
+	}
+	
+	@Override
+	public List<AttachVO> selectAttachByArticlesCode(int atchFileNo) throws Exception {
+		List<AttachVO> articles = session.selectList("Attach-Mapper.selectLaundryArticlesAttach", atchFileNo); 
+		return articles;
 	}
 	
 
---
+++ Rundering_management/src/main/java/com/rundering/dao/LaundryArticlesDAO.java
@@ -0,0 +1,21 @@
+package com.rundering.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.rundering.manage.Criteria;
+import com.rundering.dto.LaundryArticlesVO;
+
+public interface LaundryArticlesDAO {
+	List<LaundryArticlesVO> LaundryArticlesList(Criteria cri) throws SQLException;
+	
+	int selectLaundryArticlesCriteriaTotalCount(Criteria cri) throws SQLException;
+	
+	LaundryArticlesVO selectLaundryArticlesListByArticlesCode(String articlesCode) throws SQLException;
+
+	void insertLaundryArticles(LaundryArticlesVO laundryArticles) throws SQLException;
+
+	void updateLaundryArticles(LaundryArticlesVO laundryArticles) throws SQLException;
+
+	void deleteLaundryArticles(String articlesCode) throws SQLException;
+}
