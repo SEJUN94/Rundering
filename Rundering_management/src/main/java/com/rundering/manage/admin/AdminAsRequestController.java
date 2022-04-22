8dae26f9a2707e61cc4267597686949774dd7963
mino <mino@DESKTOP-MINO>
2022-04-22 12:37:01 +0900

    본사 - AS요청관리 완성

--- Rundering_management/src/main/java/com/rundering/manage/admin/AdminAsRequestController.java
+++ Rundering_management/src/main/java/com/rundering/manage/admin/AdminAsRequestController.java
@@ -1,4 +1,4 @@
-package com.rundering.manage.branch;
+package com.rundering.manage.admin;
 
 import java.sql.SQLException;
 import java.util.Map;
@@ -21,8 +21,8 @@ import com.rundering.service.AsRequestServiceImpl;
 import com.rundering.service.LaundryFixturesServiceImpl;
 
 @Controller
-@RequestMapping("/branch/asrequest")
-public class AsRequestController {
+@RequestMapping("/admin/asrequest")
+public class AdminAsRequestController {
 	@Autowired
 	AsRequestServiceImpl asRequestService;
 	
@@ -32,7 +32,7 @@ public class AsRequestController {
 	// 리스트
 	@RequestMapping(value = "/list")
 	private ModelAndView asRequestList(Criteria cri, ModelAndView mnv) throws Exception {
-		String url = "branch/asrequest/asrequest_list";
+		String url = "admin/asrequest/asrequest_list";
 
 		Map<String, Object> dataMap = asRequestService.getAsRequestList(cri);
 		mnv.addObject("dataMap", dataMap);
@@ -41,61 +41,11 @@ public class AsRequestController {
 		return mnv;
 	}
 
-	@RequestMapping("/registForm")
-	private String asRequestRegistForm() {
-
-		String url = "branch/asrequest/asrequest_regist";
-
-		return url;
-	}
-
-	@RequestMapping(value = "/regist")
-	public String asRequestRegist(AsRequestVO asRequest, HttpServletRequest request, RedirectAttributes rttr)
-			throws Exception {
-
-		String url = "redirect:/branch/asrequest/list";
-
-		asRequestService.regist(asRequest);
-		asRequestService.getItemList(asRequest);
-		
-		rttr.addAttribute("getItemList", asRequest.getArticlesCode());
-		rttr.addFlashAttribute("from", "regist");
-
-		return url;
-	}
-	
-	@RequestMapping("/modifyForm")
-	public ModelAndView modifyForm(int asno, ModelAndView mnv) throws Exception {
-
-		String url = "branch/asrequest/asrequest_modify";
-
-		AsRequestVO asRequest = asRequestService.getAsRequestModify(asno);
-	
-		mnv.addObject("asRequest", asRequest);
-		
-		mnv.setViewName(url);
-
-		return mnv;
-	}
-
-	@RequestMapping(value = "/modify", method = RequestMethod.POST)
-	public String modifyPost(AsRequestVO asRequest, HttpServletRequest request, RedirectAttributes rttr) throws Exception {
-
-		String url = "redirect:/branch/asrequest/detail";
-		
-		asRequestService.modify(asRequest);
-
-		rttr.addAttribute("asno", asRequest.getAsno());
-		rttr.addFlashAttribute("from", "modify");
-
-		return url;
-	}
-
 	@RequestMapping(value = "/detail")
 	private ModelAndView asRequestDetail(int asno, @RequestParam(defaultValue = "") String from,
 			HttpServletRequest request, ModelAndView mnv, HttpSession session) throws SQLException {
 
-		String url = "branch/asrequest/asrequest_detail";
+		String url = "admin/asrequest/asrequest_detail";
 
 		AsRequestVO asRequest = null;
 
@@ -116,18 +66,4 @@ public class AsRequestController {
 
 		return mnv;
 	}
-	
-	@RequestMapping(value = "/remove", method = RequestMethod.GET)
-	public String remove(int asno, RedirectAttributes rttr) throws Exception {
-		String url = "redirect:/branch/asrequest/detail";
-
-		asRequestService.remove(asno);
-
-		rttr.addFlashAttribute("from", "remove");
-		rttr.addAttribute("asno", asno);
-
-		return url;
-	}
-
-
 }
