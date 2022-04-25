367568cee29d41ec04abf7f33eb87facf0f0fac6
EOJIN <rornwl4512@gmail.com>
2022-04-25 13:54:10 +0900

    스케줄러 Spring Configuration File에 세팅

---
+++ Rundering_management/src/main/java/com/rundering/scheduler/OrderTaskScheduler.java
@@ -0,0 +1,15 @@
+package com.rundering.scheduler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class OrderTaskScheduler {
+	
+	private static final Logger logger = LoggerFactory.getLogger(OrderTaskScheduler.class);
+	
+	//세탁주문 지점에 할당
+	public void assignLaundryOrderToBranch() {
+		
+	}
+}
