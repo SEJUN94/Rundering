3645886ef4c46835932b8aaa7ea6b1a99331cfd3
EOJIN <rornwl4512@gmail.com>
2022-04-26 20:39:51 +0900

    본사 - 세탁주문 지점할당 스케줄링 처리중

--- Rundering_management/src/main/java/com/rundering/scheduler/OrderTaskScheduler.java
+++ Rundering_management/src/main/java/com/rundering/scheduler/OrderTaskScheduler.java
@@ -1,5 +1,6 @@
 package com.rundering.scheduler;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -40,8 +41,7 @@ public class OrderTaskScheduler {
 
 		for (ComCodeVO comCodeVO : areaCodeList) {
 			List<BranchVO> branchListByArea = branchDAO.selectBranchListByArea(comCodeVO.getComCode());
-			List<LaundryOrderVO> orderListByArea = laundryOrderDAO
-					.selectLaundryOrderListNotAssignedToBranchByArea(comCodeVO.getComCode());
+			List<LaundryOrderVO> orderListByArea = laundryOrderDAO.selectLaundryOrderListNotAssignedToBranchByArea(comCodeVO.getComCode());
 
 			if (orderListByArea.size() == 0)
 				continue;
@@ -88,47 +88,170 @@ public class OrderTaskScheduler {
 						remainingOrderQuantity -= 1;
 					}
 				}
-				System.out.println(comCodeVO.getComCodeNm() + " 지역의 담당지점 미할당 주문수 : " + remainingOrderQuantity);
+				logger.info(comCodeVO.getComCodeNm() + " 지역의 담당지점 미할당 주문수 : " + remainingOrderQuantity);
 
-				// 지역의 전체주문수가 지역내 지점들의 세탁가능수량보다 많을 때 (지역 내 지점들로 감당하지 못할 때)
-			} else {
 				
+			} else { // 지역의 전체주문수가 지역내 지점들의 세탁가능수량보다 많을 때 (지역 내 지점들로 감당하지 못할 때)
+				
+				for (BranchVO branchVO : branchListByArea) {
+					logger.info(comCodeVO.getComCodeNm() + " 지역의 전체주문수 : " + totalOrderQuantityInArea
+							+ ", 지역내모든지점의 총세탁가능량 : " + totalProcessableOrderQuantity);
+					logger.info(branchVO.getBranchName() + "의 세탁가능량 : " + branchVO.getBranchLndrpcrymslmcoqy());
+					LaundryOrderVO orderVO = new LaundryOrderVO();
+					for (int i = cnt + 0; i < cnt + branchVO.getBranchLndrpcrymslmcoqy() ; i++) {
+						orderVO = orderListByArea.get(i);
+						orderVO.setBranchCode(branchVO.getBranchCode());
+						laundryOrderDAO.updateLaundryOrderbranchCode(orderVO);
+					}
+					cnt += branchVO.getBranchLndrpcrymslmcoqy();
+					remainingOrderQuantity -= cnt;
+				}
+				logger.info(comCodeVO.getComCodeNm() + " 지역의 담당지점 미할당 주문수 : " + remainingOrderQuantity);
 			}
+		}
+		
+		//초과 주문 타 지역지점에 넘기기
+		for (ComCodeVO comCodeVO : areaCodeList) {
+			List<LaundryOrderVO> orderListByArea = laundryOrderDAO.selectLaundryOrderListNotAssignedToBranchByArea(comCodeVO.getComCode());
 
+			if (orderListByArea.size() == 0)
+				continue;
+
+			int remainingOrderQuantity = orderListByArea.size();
+			int cnt = 0;
+			int totalProcessableOrderQuantity = 0;
+			if (remainingOrderQuantity > 0) {
+				List<String> areaList = getAffordableArea(comCodeVO.getComCode(), remainingOrderQuantity);
+				if(areaList.size() == 0 ) {
+					
+					logger.info(comCodeVO.getComCodeNm() + "지역의 초과주문을 이관할 적당한 지역을 찾지 못했습니다. 확인바랍니다.");
+					
+				}else if (areaList.size() == 1) { //타지역 1곳에 할당
+					
+					List<BranchVO> branchListOfOtherArea = branchDAO.selectBranchListByArea(areaList.get(0));
+					int totalRemainingOrderQuantity = remainingOrderQuantity;
+					totalProcessableOrderQuantity = 0;
+					for (BranchVO branchVO : branchListOfOtherArea) {
+						totalProcessableOrderQuantity += branchVO.getBranchLndrpcrymslmcoqy();
+					}
+					BranchVO mostThroughputBranch = null;
+					LaundryOrderVO orderVO = new LaundryOrderVO();
+					for (BranchVO branchVO : branchListOfOtherArea) {
+						// 지점 주문할당할 개수 = 다른지역의 남은주문수 * (해당지점의 세탁가능수량 / 지역내모든지점의 세탁가능수량 * 100) / 100
+						int quantity = (int) (totalRemainingOrderQuantity * ((double) branchVO.getBranchLndrpcrymslmcoqy() / totalProcessableOrderQuantity * 100) / 100);
+						logger.info(comCodeVO.getComCodeNm() + " 지역의 초과주문수 : " + totalRemainingOrderQuantity);
+						logger.info(branchVO.getBranchName() + "의 세탁가능량 : " + branchVO.getBranchLndrpcrymslmcoqy() + ", "	+ branchVO.getBranchName() + "의 할당 퍼센트"+ ((double) branchVO.getBranchLndrpcrymslmcoqy() / totalProcessableOrderQuantity * 100));
+						logger.info(branchVO.getBranchName() + "에 할당될 "+comCodeVO.getComCodeNm() + " 지역의 주문수" + quantity);
+						if (mostThroughputBranch == null || (mostThroughputBranch.getBranchLndrpcrymslmcoqy() < branchVO.getBranchLndrpcrymslmcoqy())) {
+							mostThroughputBranch = branchVO;
+						}
+						for (int i = cnt + 0; i < cnt + quantity; i++) {
+							orderVO = orderListByArea.get(i);
+							orderVO.setBranchCode(branchVO.getBranchCode());
+							laundryOrderDAO.updateLaundryOrderbranchCode(orderVO);
+						}
+						cnt += quantity;
+						remainingOrderQuantity -= quantity;
+					}
+					if (remainingOrderQuantity > 0) {
+						for (int i = cnt + 0; i < cnt + remainingOrderQuantity; i++) {
+							orderVO = orderListByArea.get(i);
+							orderVO.setBranchCode(mostThroughputBranch.getBranchCode());
+							laundryOrderDAO.updateLaundryOrderbranchCode(orderVO);
+							remainingOrderQuantity -= 1;
+						}
+					}
+					logger.info(comCodeVO.getComCodeNm() + " 지역의 담당지점 미할당 주문수 : " + remainingOrderQuantity);
+					
+				}else if (areaList.size() == 2) { //타지역 2곳에 할당
+					
+					BranchVO mostThroughputBranch = null;
+					
+					for (String area : areaList) {
+						int totalExcessCapacity = branchDAO.selectExcessCapacityOfTodayLaundryByArea(area);
+						int excessCapacity = branchDAO.selectExcessCapacityOfTodayLaundryByArea(area);
+						if(excessCapacity >= remainingOrderQuantity) excessCapacity = remainingOrderQuantity;
+						List<BranchVO> branchListOfOtherArea = branchDAO.selectBranchListByArea(area);
+						int totalRemainingOrderQuantity = remainingOrderQuantity;
+						LaundryOrderVO orderVO = new LaundryOrderVO();
+						for (BranchVO branchVO : branchListOfOtherArea) {
+							// 지점 주문할당할 개수 = 할당받을다른지역의세탁처리초과량 * (해당지점의 남은세탁가능수량 / 지역의남은세탁처리가능수량 * 100) / 100
+							int quantity = (int) (excessCapacity * ((double) branchDAO.selectExcessCapacityOfTodayLaundryByBranchCode(branchVO.getBranchCode()) / totalExcessCapacity * 100) / 100);
+							logger.info(comCodeVO.getComCodeNm() + " 지역의 초과주문수 : " + totalRemainingOrderQuantity);
+							logger.info(branchVO.getBranchName() + "에 할당될 "+comCodeVO.getComCodeNm() + " 지역의 주문수" + quantity);
+							if (mostThroughputBranch == null || (branchDAO.selectExcessCapacityOfTodayLaundryByBranchCode(mostThroughputBranch.getBranchCode())< branchDAO.selectExcessCapacityOfTodayLaundryByBranchCode(branchVO.getBranchCode()))) {
+								mostThroughputBranch = branchVO;
+							}
+							for (int i = cnt + 0; i < cnt + quantity; i++) {
+								orderVO = orderListByArea.get(i);
+								orderVO.setBranchCode(branchVO.getBranchCode());
+								laundryOrderDAO.updateLaundryOrderbranchCode(orderVO);
+							}
+							cnt += quantity;
+							remainingOrderQuantity -= quantity;
+						}
+					}
+					if (remainingOrderQuantity > 0) {
+						LaundryOrderVO orderVO = new LaundryOrderVO();
+						for (int i = cnt + 0; i < cnt + remainingOrderQuantity; i++) {
+							orderVO = orderListByArea.get(i);
+							orderVO.setBranchCode(mostThroughputBranch.getBranchCode());
+							laundryOrderDAO.updateLaundryOrderbranchCode(orderVO);
+							remainingOrderQuantity -= 1;
+						}
+					}
+					logger.info(comCodeVO.getComCodeNm() + " 지역의 담당지점 미할당 주문수 : " + remainingOrderQuantity);
+				}
+					
+			}
 		}
 
 	}
 
-	private String getAffordableArea(String area) throws Exception {
-		List<BranchVO> firstBranchList = null;
-		List<BranchVO> secondBranchList = null;
+	private List<String> getAffordableArea(String area, int remainQuantity) throws Exception {
+		List<String> AffordableArea = new ArrayList<String>();
+		int excessCapacityOfFirstArea = 0;
+		int excessCapacityOfSecondArea = 0;
+		String firstArea = "";
+		String secondArea = "";
 		switch (area) {
 		case "0601":
-			firstBranchList = branchDAO.selectBranchListByArea("0602");
-			secondBranchList = branchDAO.selectBranchListByArea("0605");
+			firstArea = "0602";
+			secondArea = "0605";
 			break;
-//			case "0602":
-//				0601
-//				0603
-//				break;
-//			case "0603":
-//				0602
-//				0604
-//				break;
-//			case "0604":
-//				0603
-//				0605
-//				break;
-//			case "0605":
-//				0601
-//				0604
-//				break;
-
+		case "0602":
+			firstArea = "0601";
+			secondArea = "0603";
+			break;
+		case "0603":
+			firstArea = "0602";
+			secondArea = "0604";
+			break;
+		case "0604":
+			firstArea = "0603";
+			secondArea = "0605";
+			break;
+		case "0605":
+			firstArea = "0604";
+			secondArea = "0601";
+			break;
 		default:
-			break;
+			return AffordableArea;
 		}
-		return area;
-
+		excessCapacityOfFirstArea = branchDAO.selectExcessCapacityOfTodayLaundryByArea(firstArea);
+		excessCapacityOfSecondArea = branchDAO.selectExcessCapacityOfTodayLaundryByArea(secondArea);
+		if (remainQuantity > (excessCapacityOfFirstArea + excessCapacityOfSecondArea)) {
+			return AffordableArea;
+		}
+		if (remainQuantity <= excessCapacityOfFirstArea && excessCapacityOfFirstArea >= excessCapacityOfSecondArea) {
+			AffordableArea.add(firstArea);
+		} else if (remainQuantity <= excessCapacityOfSecondArea && excessCapacityOfFirstArea < excessCapacityOfSecondArea) {
+			AffordableArea.add(secondArea);
+		}else {
+			AffordableArea.add(firstArea);
+			AffordableArea.add(secondArea);
+		}
+		return AffordableArea;
 	}
 
 }
