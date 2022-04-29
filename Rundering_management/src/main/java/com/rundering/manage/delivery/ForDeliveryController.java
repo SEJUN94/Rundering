3a0f07f11738174e769cae1c82f596b19c5ee845
PC-14 <PC-14@DESKTOP-KUCU871>
2022-04-29 09:57:27 +0900

    cx

--- Rundering_management/src/main/java/com/rundering/manage/delivery/ForDeliveryController.java
+++ Rundering_management/src/main/java/com/rundering/manage/delivery/ForDeliveryController.java
@@ -6,9 +6,9 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.rundering.command.Criteria;
 import com.rundering.dto.LaundryOrderVO;
 import com.rundering.service.DeliveryService;
 
@@ -19,7 +19,7 @@ public class ForDeliveryController {
 	@Autowired
 	private DeliveryService deliveryService;
 	
-	@RequestMapping("/login")
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login() {
 		String url="/delivery/login";
 		return url;
