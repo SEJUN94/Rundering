22928b5a78400349f468f9fecbe8ccbc510947c3
PC-14 <PC-14@DESKTOP-KUCU871>
2022-05-02 14:11:27 +0900

    05/02(월)

---
+++ Rundering_customer/src/main/java/com/rundering/dao/BranchApplicationDAOImpl.java
@@ -0,0 +1,22 @@
+package com.rundering.dao;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.rundering.dto.BranchApplicationVO;
+
+public class BranchApplicationDAOImpl implements BranchApplicationDAO {
+	SqlSession session;
+
+	public void setSession(SqlSession session) {
+		this.session = session;
+	}
+
+	// 지점신청
+	@Override
+	public void branchApplicate(BranchApplicationVO bv) throws Exception {
+		session.update("BranchApplication-Mapper.insertBranchApplication",bv);
+		
+	}
+	
+
+}
