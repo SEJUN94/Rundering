ee87759a396ea7b57dd0c5f384adf838d1dac610
EOJIN <rornwl4512@gmail.com>
2022-05-04 17:19:48 +0900

    고객사이트 지점신청시 본사직원 알림 구현...테스트해야함

--- Rundering_customer/src/main/java/com/rundering/service/BranchApplicationServiceImpl.java
+++ Rundering_customer/src/main/java/com/rundering/service/BranchApplicationServiceImpl.java
@@ -1,7 +1,15 @@
 package com.rundering.service;
 
+import java.util.List;
+
 import com.rundering.dao.BranchApplicationDAO;
+import com.rundering.dao.BranchDAO;
+import com.rundering.dao.EmployeesDAO;
+import com.rundering.dao.NotificationDAO;
 import com.rundering.dto.BranchApplicationVO;
+import com.rundering.dto.BranchVO;
+import com.rundering.dto.EmployeesVO;
+import com.rundering.dto.NotificationVO;
 
 public class BranchApplicationServiceImpl implements BranchApplicationService {
 
@@ -10,11 +18,37 @@ public class BranchApplicationServiceImpl implements BranchApplicationService {
 	public void setBranchApplicationDAO(BranchApplicationDAO branchApplicationDAO) {
 		this.branchApplicationDAO = branchApplicationDAO;
 	}
+	private EmployeesDAO employeesDAO;
+	public void setEmployeesDAO(EmployeesDAO employeesDAO) {
+		this.employeesDAO = employeesDAO;
+	}
+	private BranchDAO branchDAO;
+	public void setBranchDAO(BranchDAO branchDAO) {
+		this.branchDAO = branchDAO;
+	}
+	private NotificationDAO notificationDAO;
+	public void setNotificationDAO(NotificationDAO notificationDAO) {
+		this.notificationDAO = notificationDAO;
+	}
 
 	// 지점 신청
 	@Override
 	public void branchApplicate(BranchApplicationVO bv) throws Exception {
 		branchApplicationDAO.branchApplicate(bv);
+		
+		//본사 직원들에게 알림
+		BranchVO branchVO = branchDAO.selectBranchByBranchCode("000000");
+		List<EmployeesVO> employeesList = employeesDAO.selectEmployeesByBranchCode(branchVO.getBranchCode());
+		NotificationVO notificationVO = new NotificationVO();
+			for (EmployeesVO employeesVO : employeesList) {
+				int sequence = notificationDAO.selectNotificationSequenceNextValue();
+				notificationVO.setNtcnId(String.valueOf(sequence));
+				notificationVO.setEmployeeId(employeesVO.getEmployeeId());
+				notificationVO.setNtcnknd("BA"); // 알림종류 공통코드 - 지점신청
+				notificationVO.setNtcncn(bv.getApplicateName());
+				notificationVO.setNtcnclickhourUrl("'/runderingmanage/admin/branchapplication/contract','A020300'");
+				notificationDAO.insertNotification(notificationVO);
+			}
 
 	}
 	
