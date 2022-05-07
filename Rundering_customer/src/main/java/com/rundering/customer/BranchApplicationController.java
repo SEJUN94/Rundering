93bd551ae50392cbe967d89bea5639e48eede98b
PC-14 <PC-14@DESKTOP-KUCU871>
2022-05-07 13:52:29 +0900

    05/07(토) 파일 다운로드 BranchApplicationController참조

--- Rundering_customer/src/main/java/com/rundering/customer/BranchApplicationController.java
+++ Rundering_customer/src/main/java/com/rundering/customer/BranchApplicationController.java
@@ -18,7 +18,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
